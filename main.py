ef967e6cee5dcb42998376b63420c3355b00c65c
Dhiru Kholia <dhiru.kholia@gmail.com>
2015-10-25 10:57:01 +0100

    Use CircleCI generated artifacts

--- main.py
+++ main.py
@@ -10,11 +10,6 @@ app.config['DEBUG'] = True
 # Note: We don't need to call run() since our application is embedded within
 # the App Engine WSGI application server.
 
-# url = "https://circleci.com/api/v1/project/magnumripper/JohnTheRipper?shallow=true&offset=0&limit=7"
-url = "https://api.github.com/repos/magnumripper/JohnTheRipper/commits"
-# artifact_url = "/artifacts/0/home/ubuntu/builds/JtR-MinGW-%s.zip"
-artifact_url = "/artifacts/0/home/ubuntu/builds/JtR-MinGW-%s.zip"
-
 @app.route('/')
 def hello():
     return 'Hello World!'
@@ -22,18 +17,17 @@ def hello():
 
 @app.route('/latest')
 def latest():
+    url = "https://circleci.com/api/v1/project/magnumripper/JohnTheRipper?shallow=true&offset=0&limit=7"
+    artifact_url = "/artifacts/0/home/ubuntu/builds/JtR-MinGW-%s.zip"
+
     try:
         request = urllib2.Request(url, headers={"Accept": "application/json"})
-        content = urllib2.urlopen(request).read()
-        # builds = json.loads(content)
-        commits = json.loads(content)
-        # for build in builds:
-        for commit in commits:
-            # build_url = build["build_url"]
-            # short_vcs_revision = build["vcs_revision"][:7]  # 7 is the default
-            short_vcs_revision = commit["sha"][:7]  # 7 is the default
-            # latest_build_url = build_url + (artifact_url % short_vcs_revision)
-            latest_build_url = "http://45.33.2.252:2443/JtR-sse2-%s.zip" % short_vcs_revision
+        contents = urllib2.urlopen(request).read()
+        builds = json.loads(contents)
+        for build in builds:
+            build_url = build["build_url"]
+            short_vcs_revision = build["vcs_revision"][:7]  # 7 is the default
+            latest_build_url = build_url + (artifact_url % short_vcs_revision)
             # does this build even exist?
             request = urllib2.Request(latest_build_url)
             request.get_method = lambda: 'HEAD'
@@ -51,6 +45,8 @@ def latest():
 
 @app.route('/builds')
 def builds():
+    url = "https://circleci.com/api/v1/project/magnumripper/JohnTheRipper?shallow=true&offset=0&limit=7"
+
     try:
         request = urllib2.Request(url, headers={"Accept": "application/json"})
         contents = urllib2.urlopen(request).read()
@@ -65,3 +61,29 @@ def builds():
 def page_not_found(e):
     """Return a custom 404 error."""
     return 'Sorry, nothing at this URL.', 404
+
+
+@app.route('/latest_inactive')
+def latest_inactive():
+    url = "https://api.github.com/repos/magnumripper/JohnTheRipper/commits"
+
+    try:
+        request = urllib2.Request(url, headers={"Accept": "application/json"})
+        content = urllib2.urlopen(request).read()
+        commits = json.loads(content)
+        for commit in commits:
+            short_vcs_revision = commit["sha"][:7]  # 7 is the default
+            latest_build_url = "http://45.33.2.252:2443/JtR-sse2-%s.zip" % short_vcs_revision  # Jim's machine
+            # does this build even exist?
+            request = urllib2.Request(latest_build_url)
+            request.get_method = lambda: 'HEAD'
+            try:
+                urllib2.urlopen(request)
+            except urllib2.HTTPError as e:
+                if e.code == 404:
+                    continue
+            return redirect(latest_build_url)
+    except urllib2.URLError, e:
+        print e
+
+    return 'Hello World!'
